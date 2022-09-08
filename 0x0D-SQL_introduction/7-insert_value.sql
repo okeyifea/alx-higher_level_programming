facb7de71afdbfac742e262bc4be29990d19fc88
Okey ifeanyi <101342170+okeyifea@users.noreply.github.com>
2022-09-08 13:37:43 -0700

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- insert a new row into the table first_table in my MYSQL server.
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Best School");
+-- Insert a new row into the table first_table in my MYSQL server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
