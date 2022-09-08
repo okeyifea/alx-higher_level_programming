2fc7e79b0c6e4561802930d62c1172f418418b17
Okey ifeanyi <101342170+okeyifea@users.noreply.github.com>
2022-09-08 13:39:29 -0700

    Update 8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- Display the number of record with id = 89 in the table first_table in my MYSQL server.
 SELECT COUNT(*)
-FROM 'first_table'
-WHERE 'id' = 89;
+FROM `first_table`
+WHERE `id` = 89;
